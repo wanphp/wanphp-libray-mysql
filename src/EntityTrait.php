e3f3e94b449dc3594e023125c96253722033d646
火子 <yxyong@126.com>
2021-04-16 10:32:37 +0800

    add EntityTrait.php

---
+++ src/EntityTrait.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Wanphp\Libray\Mysql;
+
+trait EntityTrait
+{
+  /**
+   * 初始化实体
+   * @param array $array
+   */
+  public function __construct(array $array)
+  {
+    foreach (array_intersect_key($array, $this->jsonSerialize()) as $key => $value) {
+      $this->{$key} = $value;
+    }
+  }
+
+  /**
+   * @param $name
+   * @param null $arguments
+   * @return mixed|null
+   */
+  public function __call($name, $arguments = null)
+  {
+    $action = substr($name, 0, 3);
+    $property = strtolower(substr($name, 3));
+    if ($action == 'set' && property_exists($this, $property)) {
+      $this->{$property} = $arguments;
+      return $arguments;
+    } elseif ($action == 'get' && property_exists($this, $property)) {
+      return $this->{$property};
+    } else {
+      return null;
+    }
+  }
+
+  public function jsonSerialize()
+  {
+    return get_object_vars($this);
+  }
+}
