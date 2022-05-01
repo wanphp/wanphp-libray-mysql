cfc57abb1dbf93d894806dfdd721dfdb84c6751c
火子 <yxyong@126.com>
2022-05-01 10:49:12 +0800

    使用Medoo2.1,'error' => PDO::ERRMODE_SILENT

--- src/BaseRepository.php
+++ src/BaseRepository.php
@@ -7,8 +7,6 @@ abstract class BaseRepository implements BaseInterface
   protected Database $db;
   protected string $tableName;
   private string $entityClass;
-  private string $errCode = '';
-  private string $errMessage = '';
 
   public function __construct(Database $database, string $tableName, string $entityClass)
   {
@@ -22,49 +20,11 @@ abstract class BaseRepository implements BaseInterface
    */
   public function insert(array $data): int
   {
-    $required = [];//必须项
-    try {
-      $class = new \ReflectionClass($this->entityClass); //建立实体类的反射类
-      $docblock = $class->getDocComment();
-      if (preg_match('/required=\{(.*?)\}/', $docblock, $primary)) {
-        $required = explode(',', str_replace(['"', '\''], '', $primary[1]));
-      }
-    } catch (\ReflectionException $exception) {
-      throw new \Exception($exception->getMessage(), $exception->getCode());
-    }
-
+    $required = $this->required();
     if (!isset($data[0])) $data = [$data];
-    foreach ($data as &$item) {
-      $fields = [];
-      foreach ($item as $key => $value) {
-        if ($pos = strpos($key, '[')) {
-          $field = substr($key, 0, $pos);
-          $item[$field] = $value;
-          unset($item[$key]);
-          $fields[$field] = $key;
-        }
-      }
+    foreach ($data as &$item) $this->checkedData($item, $required);
 
-      $item = array_filter((new $this->entityClass($item))->jsonSerialize(), function ($value, $key) use ($required) {
-        if (in_array($key, $required) && $value == '') {
-          throw new \Exception($key . ' - 不能为空');
-        }
-        return !is_null($value);
-      }, ARRAY_FILTER_USE_BOTH);
-      foreach ($fields as $key => $field) {
-        if (isset($item[$key])) {
-          $item[$field] = $item[$key];
-          unset($item[$key]);
-        }
-      }
-    }
-
-    try {
-      $this->db->insert($this->tableName, $data);
-    } catch (\Exception $e) {
-      $this->errCode = $e->getCode();
-      $this->errMessage = $e->getMessage();
-    }
+    $this->db->insert($this->tableName, $data);
     return $this->returnResult($this->db->id() ?? 0);
   }
 
@@ -73,30 +33,8 @@ abstract class BaseRepository implements BaseInterface
    */
   public function update(array $data, array $where): int
   {
-    $fields = [];
-    foreach ($data as $key => $value) {
-      if ($pos = strpos($key, '[')) {
-        $field = substr($key, 0, $pos);
-        $data[$field] = $value;
-        $fields[$field] = $key;
-        unset($data[$key]);
-      }
-    }
-    $data = array_filter((new $this->entityClass($data))->jsonSerialize(), function ($value) {
-      return !is_null($value);
-    });
-    foreach ($fields as $key => $field) {
-      if (isset($data[$key])) {
-        $data[$field] = $data[$key];
-        unset($data[$key]);
-      }
-    }
-    try {
-      $counts = $this->db->update($this->tableName, $data, $where)->rowCount();
-    } catch (\Exception $e) {
-      $this->errCode = $e->getCode();
-      $this->errMessage = $e->getMessage();
-    }
+    $this->checkedData($data, $this->required());
+    $counts = $this->db->update($this->tableName, $data, $where)->rowCount();
     return $this->returnResult($counts ?? 0);
   }
 
@@ -105,14 +43,9 @@ abstract class BaseRepository implements BaseInterface
    */
   public function select(string $columns = '*', array $where = null): array
   {
-    if ($columns != '*') $columns = explode(',', $columns);
-    try {
-      $datas = $this->db->select($this->tableName, $columns, $where);
-    } catch (\Exception $e) {
-      $this->errCode = $e->getCode();
-      $this->errMessage = $e->getMessage();
-    }
-    return $this->returnResult($datas ?? []);
+    if ($columns != '*' && strpos($columns, ',') > 0) $columns = explode(',', $columns);
+    $data = $this->db->select($this->tableName, $columns, $where);
+    return $this->returnResult($data ?? []);
   }
 
   /**
@@ -121,12 +54,7 @@ abstract class BaseRepository implements BaseInterface
   public function get(string $columns = '*', array $where = null)
   {
     if ($columns != '*' && strpos($columns, ',') > 0) $columns = explode(',', $columns);
-    try {
-      $data = $this->db->get($this->tableName, $columns, $where);
-    } catch (\Exception $e) {
-      $this->errCode = $e->getCode();
-      $this->errMessage = $e->getMessage();
-    }
+    $data = $this->db->get($this->tableName, $columns, $where);
     return $this->returnResult($data ?? []);
   }
 
@@ -135,13 +63,7 @@ abstract class BaseRepository implements BaseInterface
    */
   public function count(string $columns = '*', array $where = null): int
   {
-    try {
-      $count = $this->db->count($this->tableName, $columns, $where);
-    } catch (\Exception $e) {
-      $this->errCode = $e->getCode();
-      $this->errMessage = $e->getMessage();
-    }
-    return $count ?? 0;
+    return $this->db->count($this->tableName, $columns, $where);
   }
 
   /**
@@ -149,12 +71,7 @@ abstract class BaseRepository implements BaseInterface
    */
   public function sum(string $column, array $where = null): float
   {
-    try {
-      $total = $this->db->sum($this->tableName, $column, $where);
-    } catch (\Exception $e) {
-      $this->errCode = $e->getCode();
-      $this->errMessage = $e->getMessage();
-    }
+    $total = $this->db->sum($this->tableName, $column, $where);
     return $total ?? 0.00;
   }
 
@@ -163,12 +80,7 @@ abstract class BaseRepository implements BaseInterface
    */
   public function delete(array $where): int
   {
-    try {
-      $counts = $this->db->delete($this->tableName, $where)->rowCount();
-    } catch (\Exception $e) {
-      $this->errCode = $e->getCode();
-      $this->errMessage = $e->getMessage();
-    }
+    $counts = $this->db->delete($this->tableName, $where)->rowCount();
     return $this->returnResult($counts ?? 0);
   }
 
@@ -182,23 +94,70 @@ abstract class BaseRepository implements BaseInterface
   }
 
   /**
+   * @return array|false|string[]
+   * @throws \Exception
+   */
+  private function required()
+  {
+    $required = [];//必须项
+    try {
+      $class = new \ReflectionClass($this->entityClass); //建立实体类的反射类
+      $docblock = $class->getDocComment();
+      if (preg_match('/required=\{(.*?)\}/', $docblock, $primary)) {
+        $required = explode(',', str_replace(['"', '\''], '', $primary[1]));
+      }
+    } catch (\ReflectionException $exception) {
+      throw new \Exception($exception->getMessage(), $exception->getCode());
+    }
+    return $required;
+  }
+
+  /**
+   * @param $data
+   * @param $required
+   * @return void
+   * @throws \Exception
+   */
+  private function checkedData(&$data, $required)
+  {
+    $fields = [];
+    foreach ($data as $key => $value) {
+      if ($pos = strpos($key, '[')) {
+        $field = substr($key, 0, $pos);
+        $data[$field] = $value;
+        unset($data[$key]);
+        $fields[$field] = $key;
+      }
+    }
+
+    $data = array_filter((new $this->entityClass($data))->jsonSerialize(), function ($value, $key) use ($required) {
+      if (in_array($key, $required) && $value == '') {
+        throw new \Exception($key . ' - 不能为空');
+      }
+      return !is_null($value);
+    }, ARRAY_FILTER_USE_BOTH);
+    foreach ($fields as $key => $field) {
+      if (isset($data[$key])) {
+        $data[$field] = $data[$key];
+        unset($data[$key]);
+      }
+    }
+  }
+
+  /**
    * @param $result
    * @return mixed
    * @throws \Exception
    */
   private function returnResult($result)
   {
-    $error = $this->db->error(); //PHP8 取不到
-    if (in_array($error[1], [1146, 1054, 1062])) {//数据表不存在，或字段不存在，主键冲突,创建或更新表
+    $error = $this->db->errorInfo;
+    if (is_null($error)) return $result;
+    //数据表不存在，或字段不存在，主键冲突,创建或更新表
+    if (is_array($error) && in_array($error[1], [1146, 1054, 1062])) {
       $this->db->initTable($this->tableName, $this->entityClass);
-    } elseif ($this->errCode) {
-      if (in_array($this->errCode, ['42S02', '42S22'])) {
-        $this->db->initTable($this->tableName, $this->entityClass);
-      }
-      throw new \Exception($this->errCode . ':' . $this->errMessage);
     }
 
-    if (is_null($error[1])) return $result;
-    else throw new \Exception($error[1] . ' - ' . $this->tableName . ' ' . $error[2], $error[1]);
+    throw new \Exception($error[2], $error[1]);
   }
 }
